de17d39ea3d09298c8cfaa9984f9ea5d930104ff
Karzan Botani <karzan@botani.nu>
2023-03-11 14:41:59 +0100

    refactor: more strict eslint and tsconfig

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,14 +2,14 @@ module.exports = {
 	root: true,
 	parser: '@typescript-eslint/parser',
 	env: {
-		es2017: true,
+		es6: true,
 		node: true,
 	},
 	parserOptions: {
 		tsconfigRootDir: __dirname,
 		project: ['./configs/tsconfig.eslint.json'],
 		sourceType: 'module',
-		ecmaVersion: 2020,
+		ecmaVersion: 2021,
 	},
 	extends: [
 		'eslint:recommended',
@@ -23,17 +23,34 @@ module.exports = {
 			files: ['src/**/*.test.ts'],
 			extends: ['plugin:vitest/recommended'],
 		},
+		{
+			files: ['**/*.ts'],
+			rules: {
+				'no-shadow': 'off',
+				'no-undef': 'off',
+				'no-use-before-define': 'off',
+			},
+		},
 	],
 	rules: {
 		// JavaScript
-		'arrow-body-style': ['error'],
+		'accessor-pairs': 'off',
+		'array-bracket-spacing': 'off',
+		'array-callback-return': 'error',
+		'arrow-body-style': ['error', 'as-needed', {requireReturnForObjectLiteral: true}],
 		'no-unneeded-ternary': ['error', {defaultAssignment: false}],
-		'no-unused-expressions': ['error'],
-		'prefer-arrow-callback': ['error'],
+		'no-unused-expressions': 'error',
+		'prefer-arrow-callback': 'error',
 
 		// TypeScript
-		'@typescript-eslint/consistent-type-imports': ['error'],
-		'@typescript-eslint/no-inferrable-types': ['error'],
+		'@typescript-eslint/consistent-type-assertions': 'error',
+		'@typescript-eslint/consistent-type-definitions': 'error',
+		'@typescript-eslint/consistent-type-imports': 'error',
+		'@typescript-eslint/member-ordering': 'error',
+		'@typescript-eslint/no-inferrable-types': 'error',
+		'@typescript-eslint/no-redeclare': 'error',
+		'@typescript-eslint/no-shadow': 'warn',
+		'@typescript-eslint/no-unnecessary-type-assertion': 'error',
 		'@typescript-eslint/no-unused-vars': [
 			'warn',
 			{argsIgnorePattern: '^_', varsIgnorePattern: '^_'},
