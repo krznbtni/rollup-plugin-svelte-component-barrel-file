f523928c1c13ebd2e122a15b93fd9abb75116f64
Karzan Botani <karzan@botani.nu>
2023-03-11 22:04:18 +0100

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,78 @@
-export function helloWorld() {
-	const message = 'Hello World from my example modern npm package!';
-	return message;
+import fs from 'node:fs/promises';
+
+import glob from 'fast-glob';
+import type {Plugin} from 'rollup';
+
+function filePathToComponentName(filePath: string): string | undefined {
+	return filePath
+		.split('/')
+		.pop()
+		?.replace(/\.\w+$/, '');
 }
 
-export function goodBye() {
-	const message = 'Goodbye from my example modern npm package!';
-	return message;
+async function buildBarrelContent(config: RollupPluginSvelteComponentBarrelFile): Promise<string> {
+	const {pathToComponentsDir} = config;
+	const filePaths = await glob(`${pathToComponentsDir}/*.svelte`);
+
+	const barrelFileContent = filePaths.reduce((acc, curr) => {
+		const componentName = filePathToComponentName(curr);
+
+		let componentPath: string | undefined;
+
+		if (config.pathAliasToComponentsDir) {
+			componentPath = curr.replace(pathToComponentsDir, config.pathAliasToComponentsDir);
+		} else {
+			componentPath = curr.replace(pathToComponentsDir, '.');
+		}
+
+		if (componentName) {
+			const exportStr = `export {default as ${componentName}} from '${componentPath}';`;
+			return (acc += `${exportStr}\n`);
+		}
+
+		return acc;
+	}, '');
+
+	return barrelFileContent;
 }
 
-export default {
-	helloWorld,
-	goodBye,
-};
+async function writeNewBarrelFile(config: RollupPluginSvelteComponentBarrelFile): Promise<void> {
+	const pathToIndexFile = `${config.pathToComponentsDir}/index.ts`;
+	const barrelFileContent = await buildBarrelContent(config);
+	await fs.writeFile(pathToIndexFile, barrelFileContent);
+}
+
+async function watchComponentsDir(config: RollupPluginSvelteComponentBarrelFile): Promise<void> {
+	const watcher = fs.watch(config.pathToComponentsDir, {recursive: true});
+
+	for await (const event of watcher) {
+		if (event.eventType === 'rename' && event.filename.endsWith('.svelte')) {
+			await writeNewBarrelFile(config);
+		}
+	}
+}
+
+interface RollupPluginSvelteComponentBarrelFile {
+	/**
+	 * Use path.resolve - please
+	 */
+	pathToComponentsDir: string;
+	/**
+	 * Example: $lib/components
+	 * Description: If left empty, the import paths will be relative
+	 * i.e: ./path/to/SomeComponent.svelte
+	 */
+	pathAliasToComponentsDir?: string;
+}
+
+export default function rollupPluginSvelteComponentBarrelFile(
+	config: RollupPluginSvelteComponentBarrelFile,
+): Plugin {
+	return {
+		name: '@krznbtni/rollup-plugin-svelte-component-barrel-file',
+		async buildStart() {
+			await writeNewBarrelFile(config);
+			void watchComponentsDir(config);
+		},
+	};
+}
