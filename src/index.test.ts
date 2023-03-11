224eb3fce492f589152c3bbab2213041e7cdd848
Karzan Botani <karzan@botani.nu>
2023-03-11 13:35:39 +0100

    refactor: format all files with prettier and eslint

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,26 +1,18 @@
-import { describe, it, assert } from "vitest";
-import { helloWorld, goodBye } from "./index";
+import {describe, it, expect} from 'vitest';
+import {helloWorld, goodBye} from './index';
 
-describe("Hello World Function", () => {
-  it("should be a function", () => {
-    assert.isFunction(helloWorld);
-  });
-
-  it("should return the hello world message", () => {
-    const expected = "Hello World from my example modern npm package!";
-    const actual = helloWorld();
-    assert.equal(actual, expected);
-  });
+describe('Hello World Function', () => {
+	it('should return the hello world message', () => {
+		const expected = 'Hello World from my example modern npm package!';
+		const result = helloWorld();
+		expect(result).toBe(expected);
+	});
 });
 
-describe("Goodbye Function", () => {
-  it("should be a function", () => {
-    assert.isFunction(goodBye);
-  });
-
-  it("should return the goodbye message", () => {
-    const expected = "Goodbye from my example modern npm package!";
-    const actual = goodBye();
-    assert.equal(actual, expected);
-  });
+describe('Goodbye Function', () => {
+	it('should return the goodbye message', () => {
+		const expected = 'Goodbye from my example modern npm package!';
+		const result = goodBye();
+		expect(result).toBe(expected);
+	});
 });
